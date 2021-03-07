1ad0470e8dc32f93554c0ab889794bfc7b9dbc12
Yuriy <47811171+YUGCHR@users.noreply.github.com>
2021-03-07 21:39:43 +0000

    Add project files.

---
+++ Services/GenerateThisInstanceGuidService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundTasksQueue.Library.Services
+{
+    // вставить генерацию уникального номера в сервис констант - уже нет, оставить здесь
+    // сделать общую библиотеку для всех sln
+    public class GenerateThisInstanceGuidService
+    {
+        private readonly string _thisBackServerGuid;
+
+        public GenerateThisInstanceGuidService()
+        {
+            _thisBackServerGuid = Guid.NewGuid().ToString();
+        }
+
+        public string ThisBackServerGuid()
+        {
+            return _thisBackServerGuid;
+        }
+    }
+}
---
+++ Services/SharedDataAccess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CachingFramework.Redis.Contracts;
+using CachingFramework.Redis.Contracts.Providers;
+using Microsoft.Extensions.Logging;
+using BackgroundTasksQueue.Library.Models;
+
+namespace BackgroundTasksQueue.Library.Services
+{
+    public interface ISharedDataAccess
+    {
+        public Task SetStartConstants(EventKeyNames eventKeysSet, string checkTokenFetched);
+        public Task<EventKeyNames> FetchAllConstants();
+    }
+
+    public class SharedDataAccess : ISharedDataAccess
+    {
+        
+        private readonly ILogger<SharedDataAccess> _logger;
+        private readonly ICacheProviderAsync _cache;
+        private readonly IKeyEventsProvider _keyEvents;
+
+        public SharedDataAccess(
+            ILogger<SharedDataAccess> logger,
+            ICacheProviderAsync cache,
+            IKeyEventsProvider keyEvents)
+        {
+            _logger = logger;
+            _cache = cache;
+            _keyEvents = keyEvents;
+        }
+
+        private const string StartConstantKey = "constants";
+        private const string StartConstantField = "all";
+        private const string CheckToken = "tt-tt-tt";
+
+        public async Task SetStartConstants(EventKeyNames eventKeysSet, string checkTokenFetched)
+        {
+            if (checkTokenFetched == CheckToken)
+            {
+                await _cache.SetHashedAsync<EventKeyNames>(StartConstantKey, StartConstantField, eventKeysSet,
+                    eventKeysSet.EventKeyBackReadinessTimeDays);
+
+                _logger.LogInformation(55050, "SetStartConstants set constants (EventKeyFrom for example = {0}) in key {1}.", eventKeysSet.EventKeyFrom, "constants");
+            }
+            else
+            {
+                _logger.LogError(55070, "SetStartConstants try to set constants unsuccessfully.");
+            }
+        }
+
+        public async Task<EventKeyNames> FetchAllConstants()
+        {
+            // 
+            return await _cache.GetHashedAsync<EventKeyNames>(StartConstantKey, StartConstantField);
+        }
+    }
+}
